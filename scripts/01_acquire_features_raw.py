713f4929d2e0d141e0e6a87c57c83f551d89f565
Ray Pomponio <raypomponio@Rays-MacBook-Pro.local>
2020-04-01 19:03:38 -0400

    first attempt at feature collection, automatically scraped from IMDb based on title, year, rating

--- scripts/01_acquire_features_raw.py
+++ scripts/01_acquire_features_raw.py
@@ -3,75 +3,129 @@ from imdb import IMDb
 import numpy as np
 import pandas as pd
 
-# read list of ratings
-df_ratings = pd.read_csv('../data/IMDB_Top250_UID_title_rating.csv')
+# load ratings
+df_ratings = pd.read_csv('../data/Personal_Movie_Ratings_Updated.csv')
 
-# define list of high-level features
-features_hl = ['aspect ratio', 'box office', 'countries', 'directors', 'genres',
-    'rating', 'runtimes', 'title', 'votes', 'writers', 'year']
+# drop movies missing ratings
+df_ratings = df_ratings.loc[~df_ratings['My Rating'].isnull(), :].reset_index(drop=True)
 
-# define list of low-level features
-features_ll = {'box office':['Budget', 'Cumulative Worldwide Gross'],
-    'countries': [0], 'directors':[0], 'genres': [0, 1, 2],
-    'runtimes':[0], 'writers': [0, 1, 2]}
+# create an instance of the IMDb class
+ia = IMDb()
 
-# define function to acquire features
-def acquire_features(movie_id, features_hl=features_hl, features_ll=features_ll):
-    """returns named list of desired features"""
-    # create an instance of the IMDb class
-    ia = IMDb()
-    movie = ia.get_movie(movie_id)
-    result = [movie.movieID]
-    names = ['UID']
-    # check existence of each high-level feature
-    for feature_hl in features_hl:
-        if not feature_hl in movie.keys():
-            raise ValueError('Feature: %s not found in movie: %s' % (feature_hl, movie_id))
+# initialize results container
+results = {'UID':[], 'My_Rating':[],
+           'Budget':[], 'Cumulative_Worldwide_Gross':[],
+           'Production_Company1':[], 'Synopsis': [],
+           'Top_250_Rank':[],
+           'Country1':[], 'Director1':[], 'Genre1': [],
+           'Genre2':[], 'Genre3':[], 'Rating':[],
+           'Runtime':[], 'Title':[], 'Poster_URL': [], 'Votes':[],
+           'Writer1':[], 'Writer2':[], 'Year':[]}
+movies_missing_uid = {'Movie': []}
 
-        if not feature_hl in features_ll.keys():
-            result.append(movie[feature_hl])
-            names.append(feature_hl)
-    # obtain low-level features and append to list
-    for feature_hl in features_ll:
-        feature_object = movie[feature_hl]
-        feature_keys = features_ll[feature_hl]
-        for key in feature_keys:
-            if isinstance(feature_object, dict):
-                if not key in feature_object.keys():
-                    result.append(np.nan)
-                else:
-                    result.append(feature_object[key])
-                names.append(key)
-            else:
-                if key>=len(feature_object):
-                    result.append(np.nan)
-                    names.append(feature_hl)
-                elif isinstance(feature_object[key], imdb.Person.Person):
-                    if 'name' in feature_object[key].keys():
-                        result.append(feature_object[key]['name'])
-                    else:
-                        result.append(np.nan)
-                    names.append(feature_hl)
-                else:
-                    result.append(feature_object[key])
-                    names.append(feature_hl)
-    result = pd.Series(result)
-    result.index = names
-    return result
-
-# iterate over movie list
+# iterate over movies
 for i in range(0, df_ratings.shape[0]):
-    print(i)
-    try:
-        result = acquire_features(df_ratings.UID[i])
-    except:
-        print('exception occurred for: %s, skipping movie!' % df_ratings.Title[i])
+    my_movie_title = df_ratings.Movie[i]
+    my_movie_year = df_ratings['Release Year'][i]
+    my_movie_imdb = df_ratings['IMDb'][i] / 10
+    my_movie_rating = df_ratings['My Rating'][i]
+    # search by title first
+    title_search = ia.search_movie(my_movie_title)
+    # is the first item in search the correct movie?
+    movie_guess = ia.get_movie(title_search[0].movieID)
+    # does the first item in the search match the release year?
+    if movie_guess['year']!=my_movie_year:
+        print('Skipping movie due to mismatch in year: %s' % my_movie_title)
+        movies_missing_uid['Movie'].append(my_movie_title)
         continue
-    if i==0:
-        df_results = result.to_frame().T
-    else:
-        df_result = result.to_frame().T
-        df_results = pd.concat((df_results, df_result), sort=False, ignore_index=True)
+    # does the first item in the search match the rating?
+    if (np.abs(movie_guess['rating'] - my_movie_imdb) > 0.5):
+        print('Skipping movie due to mismatch in rating: %s' % my_movie_title)
+        movies_missing_uid['Movie'].append(my_movie_title)
+        continue
+    # for the movie object, try to obtain all features
+    results['UID'].append(movie_guess.movieID)
+    results['My_Rating'].append(my_movie_rating)
+    try:
+        results['Budget'].append(movie_guess['box office']['Budget'])
+    except:
+        results['Budget'].append('')
+    try:
+        results['Cumulative_Worldwide_Gross'].append(movie_guess['box office']['Cumulative Worldwide Gross'])
+    except:
+        results['Cumulative_Worldwide_Gross'].append('')
+    try:
+        results['Production_Company1'].append(movie_guess['production companies'][0]['name'])
+    except:
+        results['Production_Company1'].append('')
+    try:
+        results['Synopsis'].append(movie_guess['synopsis'][0])
+    except:
+        results['Synopsis'].append('')
+    try:
+        results['Top_250_Rank'].append(movie_guess['top 250 rank'])
+    except:
+        results['Top_250_Rank'].append('')
+    try:
+        results['Country1'].append(movie_guess['countries'][0])
+    except:
+        results['Country1'].append('')
+    try:
+        results['Director1'].append(movie_guess['directors'][0]['name'])
+    except:
+        results['Director1'].append('')
+    try:
+        results['Genre1'].append(movie_guess['genres'][0])
+    except:
+        results['Genre1'].append('')
+    try:
+        results['Genre2'].append(movie_guess['genres'][1])
+    except:
+        results['Genre2'].append('')
+    try:
+        results['Genre3'].append(movie_guess['genres'][2])
+    except:
+        results['Genre3'].append('')
+    try:
+        results['Rating'].append(movie_guess['rating'])
+    except:
+        results['Rating'].append('')
+    try:
+        results['Runtime'].append(movie_guess['runtimes'][0])
+    except:
+        results['Runtime'].append('')
+    try:
+        results['Title'].append(movie_guess['title'])
+    except:
+        results['Title'].append('')
+    try:
+        results['Poster_URL'].append(movie_guess['full-size cover url'])
+    except:
+        results['Poster_URL'].append('')
+    try:
+        results['Votes'].append(movie_guess['votes'])
+    except:
+        results['Votes'].append('')
+    try:
+        results['Writer1'].append(movie_guess['writer'][0]['name'])
+    except:
+        results['Writer1'].append('')
+    try:
+        results['Writer2'].append(movie_guess['writer'][1]['name'])
+    except:
+        results['Writer2'].append('')
+    try:
+        results['Year'].append(movie_guess['year'])
+    except:
+        results['Year'].append('')
+
 
 # write to csv
-df_results.to_csv('../data/IMDB_Top250_features_raw.csv', index=False)
+df_results = pd.DataFrame(results)
+df_results.to_csv('../data/Personal_Movies_Ratings_Features.csv', index=False)
+
+# write missing movies to separate csv
+df_missing = pd.DataFrame(movies_missing_uid)
+df_missing.to_csv('../data/Personal_Movies_Missing_UID.csv', index=False)
+
+
